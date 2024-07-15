b27ddc7102da0e46117dbcb78a867930d402b888
Itzshubu <shubuchoudhary1500@gmail.com>
2024-07-15 21:12:14 +0530

    adding the dotenv packeg

--- main.js
+++ main.js
@@ -1,58 +1,55 @@
-const express = require('express')
-const axios = require('axios');
-const cors = require('cors')
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+require("dotenv").config();
 
+const app = express();
+const port = 3000;
 
-const app = express()
-const port = 3000
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
-app.use(cors({
-  origin:"http://localhost:3000"
-}));
-
-app.use('/src',express.static('src'));
-app.set('view engine', 'ejs');
+app.use("/src", express.static("src"));
+app.set("view engine", "ejs");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.render("Index");
+  // res.send(`${process.env.API_KEY}`)
+});
 
 
+app.post("/searching", async (req, res) => {
+  let url = req.body.text;
+  const options = {
+    method: "GET",
+    url: "https://instagram-api39.p.rapidapi.com/instagram/",
+    params: {
+      url: url,
+    },
+    headers: {
+      "X-RapidAPI-Key": `${process.env.API_KEY}`,
+      "X-RapidAPI-Host": "instagram-api39.p.rapidapi.com",
+    },
+  };
 
-app.get('/', (req, res) => {
-
-res.render('Index');
-})
-
-
-app.post('/searching', async (req, res) => { 
-
-let url = req.body.text;
-const options = {
-  method: 'GET',
-  url: 'https://instagram-api39.p.rapidapi.com/instagram/',
-  params: {
-    url: url
-  },
-  headers: {
-    'X-RapidAPI-Key': `${process.env.API_KEY}`,
-    'X-RapidAPI-Host': 'instagram-api39.p.rapidapi.com'
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+    const data = response.data;
+    console.log(data);
+    res.json(data);
+  } catch (error) {
+    res.send("plese try agane later");
+    console.error(error);
   }
-};
-
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-  const data = response.data;
-  console.log(data)
-  res.json(data);
-
-} catch (error) {
-  res.send("plese try agane later")
-	console.error(error);
-}
-})
-
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- testing the api usind js.js
+++ testing the api usind js.js
@@ -1,22 +1,22 @@
-const url = 'https://instagram-api39.p.rapidapi.com/instagram/?url=https://www.instagram.com/p/C6RV8khvjnM/?utm_source=ig_web_copy_link';
+require("dotenv").config();
+console.log(process.env.API_KEY)
+
+async function main(){
+const url = 'https://instagram-api39.p.rapidapi.com/instagram/?url=https://www.instagram.com/p/CpiWA1fvro_/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==';
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'your-rapid-api-key',
-		'X-RapidAPI-Host': 'instagram-api39.p.rapidapi.com'
+		'x-rapidapi-key': `${process.env.API_KEY}`,
+		'x-rapidapi-host': 'instagram-api39.p.rapidapi.com'
 	}
 };
 
-
-async function main(){
-
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        console.log(result);
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+	console.log(result);
 } catch (error) {
 	console.error(error);
 }
-
 }
 main()
